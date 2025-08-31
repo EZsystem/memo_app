bd18de3ff151ba9165d7ac612cdaaba388b96abf
EZsystem <1974eiji2@gmail.com>
2025-08-31 14:18:34 +0900

    更新処理機能追加

--- resources/views/livewire/memos/show.blade.php
+++ resources/views/livewire/memos/show.blade.php
@@ -6,10 +6,17 @@ use App\Models\Memo;
 //ルートモデルバインディング
 state(['memo' => fn(Memo $memo) => $memo]);
 
+// 編集ページへリダイレクトする関数
+$edit = function () {
+    return redirect()->route('memos.edit', $this->memo);
+};
+
 ?>
 
 <div>
     <a href="{{ route('memos.index') }}">戻る</a>
     <h1>{{ $memo->title }}</h1>
     <p>{!! nl2br(e($memo->body)) !!}</p>
+
+    <button wire:click="edit">編集する</button>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,11 @@ Route::get('/', function () {
 
 // 一覧ページ
 Volt::route('/memos', 'memos.index')->name('memos.index');
+// 具体的なパスを先に設定
+// 新規作成ページ
+Volt::route('/memos/create', 'memos.create')->name('memos.create');
+// 同出来なパラメータを含むルートは最後に設定
+// 詳細ページ
 Volt::route('/memos/{memo}', 'memos.show')->name('memos.show');
+// 編集ページ
+Volt::route('/memos/{memo}/edit', 'memos.edit')->name('memos.edit');
