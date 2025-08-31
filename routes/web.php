1882f97759b4bb5e961c8bd6d1e664a43a4d9030
EZsystem <1974eiji2@gmail.com>
2025-08-31 10:48:58 +0900

    紹介機能の作成

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Volt::route('/memos/{memo}', 'memos.show')->name('memos.show');
