49f7a913f7d8424c24d958060bfb5f8f413ec00e
EZsystem <1974eiji2@gmail.com>
2025-08-31 16:17:33 +0900

    エラーメッセージの装飾

--- resources/views/livewire/memos/edit.blade.php
+++ resources/views/livewire/memos/edit.blade.php
@@ -1,17 +1,23 @@
 <?php
 
-use function Livewire\Volt\{state, mount};
+use function Livewire\Volt\{state, mount, rules};
 use App\Models\Memo;
 
 state(['memo', 'title', 'body']);
 
-mount(function(Memo $memo){
+mount(function (Memo $memo) {
     $this->memo = $memo;
     $this->title = $memo->title;
     $this->body = $memo->body;
 });
 
-$update = function() {
+rules([
+    'title' => 'required|string|max:50',
+    'body' => 'required|string|max:2000',
+]);
+
+$update = function () {
+    $this->validate(); //バリエーションチェック
     $this->memo->update($this->all());
     return redirect()->route('memos.show', $this->memo);
 };
@@ -23,10 +29,18 @@ $update = function() {
     <form wire:submit="update">
         <p>
             <label for="title">タイトル</label><br>
+            @error('title')
+                <span class="error">({{ $message }})</span>
+            @enderror
+            <br>
             <input type="text" wire:model="title" id="title">
         </p>
         <p>
             <label for="body">本文</label><br>
+            @error('body')
+                <span class="error">({{ $message }})</span>
+            @enderror
+            <br>
             <textarea wire:model="body" id="body"></textarea>
         </p>
         <button type="submit">更新</button>
